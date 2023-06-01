f579d385315c2a55ddac95a476cfca79f9da24dd
ziniminisoldier <philipp.karst@gmx.de>
2023-06-01 12:33:51 +0200

    Probeklausur

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,9 +1,20 @@
 package com.cc.java;
 
 public class App {
+//     private int counter;   
+//     public static void main(String[] args) {
+//     }
     
-    public static void main(String[] args) {
-     
-    }
-    
+//     public int getcounter(){
+//         return counter;
+//     }
+//     public void setcounter(){
+//         this.counter = counter;
+//     }
+
+
+
+
 }
+
+
